b356384d2753b11073dd70ce5aa7507e46f217ae
Sadaf Saleem <sadafsaleem5815@gmail.com>
2023-03-31 21:25:13 +0500

    Create classes.py

---
+++ 2# Class,instances/classes.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__ (self, first, last):
+        self.first = first
+        self.last = last
+
+emp_1 = Employee('Nobita', 'Ahmed')
+emp_2 = Employee('Shezooka', 'Ahmed') 
+
+#print(emp_1)
+#print(emp_2)
+
+
+print(emp_1.first)
+print(emp_1.last)
+print(emp_2.first)
+print(emp_2.last)
+
+
+
+
+
+
+
